2afea25ec06d8ab2eb81dd53d795f6afc7b3881d
Hendrik Oenings <caylee@caylee.de>
2024-06-18 21:53:30 +0200

    Add example compose file

--- README.md
+++ README.md
@@ -11,6 +11,21 @@ cd masterportal-cloud
 docker build -t masterportal .
 ```
 
+A portal is not provided with the container.
+It needs to be mounted as a volume inside `/srv/www` (see below).
+
+The web server is exposed at port 80 in the container.
+
+## Using docker compose
+`docker compose` allows to pre-configure the environment of the docker container.
+This repository provides an example configuration that loads the demo portal from the `portal` folder and exposes the Masterportal at port 8080 on your machine.
+
+Usage:
+```
+docker compose build
+docker compose up -d
+```
+
 ## Future plans
 
 - Provide a Helm chart for Kubernetes deployments
---
+++ portal/config.js
@@ -0,0 +1,29 @@
+const Config = {
+	alerting: {
+		fetchBroadcastUrl: './resources/newsFeedPortalAlerts.json',
+	},
+	namedProjections: [
+		[ 'EPSG:25832', '+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs' ],
+	],
+	layerConf: './resources/services-internet.json',
+	restConf: './resources/rest-services-internet.json',
+	styleConf: './resources/style_v3.json',
+	wfsImgPath: './resources/img/',
+	portalLanguage: {
+		enabled: true,
+		debug: false,
+		languages: {
+			de: 'Deutsch',
+			en: 'English',
+			es: 'Español',
+			it: 'Italiano',
+			platt: 'Platt',
+			pt: 'Português',
+			ru: 'Русский',
+			tr: 'Türkçe',
+			ua: 'Українська',
+		},
+		fallbackLanguage: 'de',
+		changeLanguageOnStartWhen: [ 'querystring', 'localStorage', 'htmlTag' ],
+	},
+}
